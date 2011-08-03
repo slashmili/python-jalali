6bdd957c4963e4f68317c54f2d9f4907092d79fb
Milad Rastian <milad@yournex.org>
2011-08-03 20:19:39 +0800

    increase version to 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name='jdatetime',
-        version='1.0',
+        version='1.1',
         packages=['jdatetime',],
         license='Python Software Foundation License',
         keywords='Jalali implementation of Python datetime',
