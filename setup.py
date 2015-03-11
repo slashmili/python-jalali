9fdddc1f537a30d6c18fe2f373e61ab92c9fc384
Low Kian Seong <kianseong@gmail.com>
2015-03-11 12:30:09 +0800

    Adding metadata to setup.py

--- setup.py
+++ setup.py
@@ -8,4 +8,7 @@ setup(
         keywords='Jalali implementation of Python datetime',
         platforms='any',
         long_description=open('README.txt').read(),
+        description=("Jalali Date support for Django 1.7 "
+                     "model and admin interface"),
+        url="https://github.com/slashmili/python-jalali",
 )
