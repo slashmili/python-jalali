2806bbf1d2704cdc92206ac44b1ababa5af899b3
Milad Rastian <eslashmili@gmail.com>
2015-06-27 14:37:49 +0800

    Updated desc

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
         keywords='Jalali implementation of Python datetime',
         platforms='any',
         long_description=open('README.txt').read(),
-        description=("Jalali Date support for Django 1.7 "
-                     "model and admin interface"),
+        description=("Jalali datetime binding for python"),
         url="https://github.com/slashmili/python-jalali",
 )
