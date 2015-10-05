5f1cca0a0ac92c52811650b9539b0f04346252ed
Milad Rastian <eslashmili@gmail.com>
2015-10-05 19:49:18 +0800

    A failed attemp to show the readme in Markdown in pypi :(

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from distutils.core import setup
 
 setup(
         name='jdatetime',
-        version='1.7',
+        version='1.7.1',
         packages=['jdatetime',],
         license='Python Software Foundation License',
         keywords='Jalali implementation of Python datetime',
         platforms='any',
-        long_description=open('README.txt').read(),
         description=("Jalali datetime binding for python"),
         url="https://github.com/slashmili/python-jalali",
 )
