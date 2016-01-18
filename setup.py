7de0f15659bde99311443917a3e76ba7d414e275
Milad Rastian <milad@sam-media.com>
2016-01-18 13:56:56 +0800

    Bumped the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name='jdatetime',
-        version='1.7.1',
+        version='1.7.2',
         packages=['jdatetime',],
         license='Python Software Foundation License',
         keywords='Jalali implementation of Python datetime',
