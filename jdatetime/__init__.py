8c1de2aed78e266004e4fafb1c831afdbd99b434
Milad Rastian <eslashmili@gmail.com>
2016-02-23 21:00:55 +0800

    Prepare for release

--- jdatetime/__init__.py
+++ jdatetime/__init__.py
@@ -10,7 +10,7 @@ from jdatetime.jalali import \
     GregorianToJalali, JalaliToGregorian, j_days_in_month
 import re as _re
 import locale as _locale
-__VERSION__ = "1.7.1"
+__VERSION__ = "1.7.4"
 MINYEAR = 1
 MAXYEAR = 9377
 
