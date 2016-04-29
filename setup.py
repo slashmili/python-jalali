37ffca06f98b92981e7f7624359f7b17b6a8c5cb
Milad Rastian <eslashmili@gmail.com>
2016-04-29 17:19:22 +0800

    Bump the version to 1.8.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name='jdatetime',
-        version='1.8.0',
+        version='1.8.1',
         packages=['jdatetime',],
         license='Python Software Foundation License',
         keywords='Jalali implementation of Python datetime',
