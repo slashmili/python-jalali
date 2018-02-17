eb8c9f913c56848aa116ce30a2124f599058608e
Milad Rastian <milad@high-mobility.com>
2018-02-17 19:06:30 +0100

    Release 1.9.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name='jdatetime',
-        version='1.9.0',
+        version='1.9.1',
         packages=['jdatetime',],
         license='Python Software Foundation License',
         keywords='Jalali implementation of Python datetime',
