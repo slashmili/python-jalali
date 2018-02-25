c958eb41b107888e3ab6d7d473d12f8f844a26ba
Milad Rastian <milad@high-mobility.com>
2018-02-25 12:14:08 +0100

    bump version to 2.0.0

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
         name='jdatetime',
-        version='1.9.1',
+        version='2.0.0',
         packages=['jdatetime',],
         license='Python Software Foundation License',
         keywords='Jalali implementation of Python datetime',
         platforms='any',
         author = 'Milad Rastian',
-        author_email = 'eslashmili _at_ gmail.com',
+        author_email = 'eslashmili@gmail.com',
         description=("Jalali datetime binding for python"),
         url="https://github.com/slashmili/python-jalali",
         long_description=open('README').read()
