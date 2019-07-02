81085c68d741a8072d55042a281d45c1487ad477
Meysam Azad <MeysamAzad81@yahoo.com>
2019-07-02 11:44:01 +0430

    add dependencies to setup

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from distutils.core import setup
 
+REQUIRES = (
+        'six==1.12.0',
+)
+
 setup(
         name='jdatetime',
         version='3.2.0',
@@ -8,6 +12,7 @@ setup(
         keywords='Jalali implementation of Python datetime',
         platforms='any',
         author = 'Milad Rastian',
+        install_requires=REQUIRES,
         author_email = 'eslashmili@gmail.com',
         description=("Jalali datetime binding for python"),
         url="https://github.com/slashmili/python-jalali",
