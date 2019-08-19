115d589eee5f4683c33a858bb8e0d0d969afc4b1
Pouya MN <pouya.malekinejad@gmail.com>
2019-08-19 21:58:27 +0430

    added test for mixed digits

--- t/test.py
+++ t/test.py
@@ -395,6 +395,14 @@ class TestJDateTime(unittest.TestCase):
         self.assertEqual(True, dt1 == dt2)
 
     def test_strptime_persian(self):
+        date_string = u"۱۳۶۳-۶-۶ ۱۲:۱۳:۱۴"
+        date_format = "%Y-%m-%d %H:%M:%S"
+        dt1 = jdatetime.datetime.strptime(date_string, date_format)
+        dt2 = jdatetime.datetime(1363, 6, 6, 12, 13, 14)
+
+        self.assertEqual(True, dt1 == dt2)
+
+    def test_strptime_persian_mixed(self):
         date_string = u"۱۳۶۳-۶-۶ 1۲:۱3:۱۴"
         date_format = "%Y-%m-%d %H:%M:%S"
         dt1 = jdatetime.datetime.strptime(date_string, date_format)
