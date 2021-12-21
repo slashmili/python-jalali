e0e777f01587ed12e426bd64e70e1ec610aed6ce
MojixCoder <mojix1010@gmail.com>
2021-12-21 12:26:47 +0330

    Make test simpler
    Co-authored-by: Hasan Ramezani <hasan.r67@gmail.com>

--- t/test.py
+++ t/test.py
@@ -90,12 +90,11 @@ class TestJDate(unittest.TestCase):
         self.assertEqual(date.strftime('%A'), u'شنبه')
 
     def test_strftime_fa_month(self):
-        j_months_fa = jdatetime.date.j_months_fa
-        month = 0
-        for date in range(12):
-            month += 1
-            j_month_fa = jdatetime.date(year=1400, month=month, day=10).strftime("%E")
-            self.assertEqual(j_month_fa, j_months_fa[month - 1])
+        for month in range(1, 13):
+            self.assertEqual(
+                jdatetime.date(year=1400, month=month, day=10).strftime("%E"),
+                jdatetime.date.j_months_fa[month - 1],
+            )
 
     def test_dates_are_not_equal_if_locales_are_different(self):
         date_fa = jdatetime.date(1397, 4, 22, locale='fa_IR')
