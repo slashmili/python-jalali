841b21aa6c853aeb43fde3b6d555e0333c13eb18
Hasan Ramezani <hasan.r67@gmail.com>
2022-02-11 16:40:36 +0100

    Fix some assertions.

--- tests/test_jdatetime.py
+++ tests/test_jdatetime.py
@@ -128,22 +128,22 @@ class TestJDateTime(TestCase):
 
     def test_comparison(self):
         today = jdatetime.date.today()
-        self.assertEqual(False, today < today - jdatetime.timedelta(days=76))
-        self.assertEqual(False, today <= today - jdatetime.timedelta(days=1))
-        self.assertEqual(True, today + jdatetime.timedelta(days=1) > today)
-        self.assertEqual(True, today + jdatetime.timedelta(days=30) >= today)
-        self.assertEqual(True, today == today)
-        self.assertEqual(False, today > today)
-        self.assertEqual(False, today < today)
-        self.assertEqual(True, today >= today)
-        self.assertEqual(True, today <= today)
+        self.assertFalse(today < today - jdatetime.timedelta(days=76))
+        self.assertFalse(today <= today - jdatetime.timedelta(days=1))
+        self.assertTrue(today + jdatetime.timedelta(days=1) > today)
+        self.assertTrue(today + jdatetime.timedelta(days=30) >= today)
+        self.assertTrue(today == today)
+        self.assertFalse(today > today)
+        self.assertFalse(today < today)
+        self.assertTrue(today >= today)
+        self.assertTrue(today <= today)
         not_today = jdatetime.date(today.year, today.month, today.day) + jdatetime.timedelta(days=1)
-        self.assertEqual(True, today != not_today)
+        self.assertTrue(today != not_today)
 
         dtg = jdatetime.datetime(1380, 12, 1, 1, 2, 4)
-        self.assertEqual(True, dtg < dtg + jdatetime.timedelta(seconds=1))
-        self.assertEqual(True, dtg - jdatetime.timedelta(seconds=1) < dtg)
-        self.assertEqual(False, dtg is None)
+        self.assertTrue(dtg < dtg + jdatetime.timedelta(seconds=1))
+        self.assertTrue(dtg - jdatetime.timedelta(seconds=1) < dtg)
+        self.assertFalse(dtg is None)
 
     def test_date_conversion_date_input(self):
         # todo: add some corner cases
@@ -173,7 +173,7 @@ class TestJDateTime(TestCase):
         )
 
         gdatetime = datetime.datetime(2011, 5, 13, 14, 15, 16)
-        self.assertEqual(True, jd_datetime.togregorian() == gdatetime)
+        self.assertEqual(jd_datetime.togregorian(), gdatetime)
 
     def test_strftime(self):
         s = jdatetime.date(1390, 2, 23)
@@ -209,11 +209,11 @@ class TestJDateTime(TestCase):
 
         nyc = NYCTime()
         dt = jdatetime.datetime(1389, 2, 17, 19, 10, 2, tzinfo=nyc)
-        self.assertEqual(True, dt.strftime("%Z %z") == "EDT -0400")
+        self.assertEqual(dt.strftime("%Z %z"), "EDT -0400")
 
         teh = TehranTime()
         dt = jdatetime.datetime(1389, 2, 17, 19, 10, 2, tzinfo=teh)
-        self.assertEqual(True, dt.strftime("%Z %z") == "IRDT +0330")
+        self.assertEqual(dt.strftime("%Z %z"), "IRDT +0330")
 
     def test_strftime_unicode(self):
         s = jdatetime.date(1390, 2, 23)
@@ -228,13 +228,13 @@ class TestJDateTime(TestCase):
 
     def test_kabiseh(self):
         kabiseh_year = jdatetime.date.fromgregorian(date=datetime.date(2013, 3, 20))
-        self.assertEqual(True, kabiseh_year.isleap() is True)
+        self.assertTrue(kabiseh_year.isleap())
 
         normal_year = jdatetime.date.fromgregorian(date=datetime.date(2014, 3, 20))
-        self.assertEqual(True, normal_year.isleap() is False)
+        self.assertFalse(normal_year.isleap())
 
         kabiseh_year = jdatetime.date(1391, 12, 30)
-        self.assertEqual(True, kabiseh_year.isleap() is True)
+        self.assertTrue(kabiseh_year.isleap())
 
         with self.assertRaises(ValueError):
             jdatetime.date(1392, 12, 30)
@@ -242,29 +242,29 @@ class TestJDateTime(TestCase):
     def test_datetime(self):
         d = jdatetime.datetime(1390, 1, 2, 12, 13, 14)
 
-        self.assertEqual(True, d.time() == jdatetime.time(12, 13, 14))
-        self.assertEqual(True, d.date() == jdatetime.date(1390, 1, 2))
+        self.assertEqual(d.time(), jdatetime.time(12, 13, 14))
+        self.assertEqual(d.date(), jdatetime.date(1390, 1, 2))
 
     def test_datetimetoday(self):
         jnow = jdatetime.datetime.today()
         today = datetime.datetime.today().date()
         gnow = jdatetime.date.fromgregorian(date=today)
 
-        self.assertEqual(True, jnow.date() == gnow)
+        self.assertEqual(jnow.date(), gnow)
 
     def test_datetimefromtimestamp(self):
         t = time.time()
         jnow = jdatetime.datetime.fromtimestamp(t).date()
         gnow = datetime.datetime.fromtimestamp(t).date()
 
-        self.assertEqual(True, jdatetime.date.fromgregorian(date=gnow) == jnow)
+        self.assertEqual(jdatetime.date.fromgregorian(date=gnow), jnow)
 
     def test_combine(self):
         t = jdatetime.time(12, 13, 14)
         d = jdatetime.date(1390, 4, 5)
         dt = jdatetime.datetime(1390, 4, 5, 12, 13, 14)
 
-        self.assertEqual(True, jdatetime.datetime.combine(d, t) == dt)
+        self.assertEqual(jdatetime.datetime.combine(d, t), dt)
 
     def test_combine_keeps_date_locale(self):
         t = jdatetime.time(11, 20, 30)
@@ -285,7 +285,7 @@ class TestJDateTime(TestCase):
         dtr = dt.replace(**args)
         dtn = jdatetime.datetime(1390, 12, 1, 13, 14, 15, 1233)
 
-        self.assertEqual(True, dtr == dtn)
+        self.assertEqual(dtr, dtn)
 
     def test_replace_keeps_date_locale(self):
         dt = jdatetime.datetime(1397, 4, 24, locale='nl_NL')
@@ -304,7 +304,7 @@ class TestJDateTime(TestCase):
         dt1 = jdatetime.datetime.strptime(date_string, date_format)
         dt2 = jdatetime.datetime(1363, 6, 6, 12, 13, 14)
 
-        self.assertEqual(True, dt1 == dt2)
+        self.assertEqual(dt1, dt2)
 
     def test_strptime_bare(self):
         date_string = "13630606121314"
@@ -366,14 +366,14 @@ class TestJDateTime(TestCase):
         date_string = "1364-6-6 12:13:14"
         dt2 = jdatetime.datetime.strptime(date_string, date_format)
 
-        self.assertEqual(False, dt2 == dt1)
+        self.assertNotEqual(dt2, dt1)
 
     def test_datetime_eq_now(self):
         import time
         dt1 = jdatetime.datetime.now()
         time.sleep(0.1)
         dt2 = jdatetime.datetime.now()
-        self.assertEqual(False, dt2 == dt1)
+        self.assertNotEqual(dt2, dt1)
 
     def test_timetz(self):
         teh = TehranTime()
@@ -387,13 +387,13 @@ class TestJDateTime(TestCase):
 
         dt_gmt = datetime.datetime(2015, 6, 27, 0, 0, 0, tzinfo=gmt)
         dt_teh = datetime.datetime(2015, 6, 27, 3, 30, 0, tzinfo=teh)
-        self.assertEqual(True, dt_teh == dt_gmt, "In standrd python datetime, __eq__ considers timezone")
+        self.assertEqual(dt_teh, dt_gmt, "In standrd python datetime, __eq__ considers timezone")
 
         jdt_gmt = jdatetime.datetime(1389, 2, 17, 0, 0, 0, tzinfo=gmt)
 
         jdt_teh = jdatetime.datetime(1389, 2, 17, 3, 30, 0, tzinfo=teh)
 
-        self.assertEqual(True, jdt_teh == jdt_gmt)
+        self.assertEqual(jdt_teh, jdt_gmt)
 
     def test_datetimes_with_different_locales_are_not_equal(self):
         dt_en = jdatetime.datetime(2018, 4, 15, 0, 0, 0, locale='en_US')
@@ -683,7 +683,7 @@ class TestJDateTime(TestCase):
 
         with open('tests/pickled_objects/%s' % pickled_object_file, 'rb') as f:
             dt = pickle.load(f)
-        assert dt == jdatetime.datetime(1400, 10, 11, 1, 2, 3, 30)
+        self.assertEqual(dt, jdatetime.datetime(1400, 10, 11, 1, 2, 3, 30))
 
 
 class TestJdatetimeGetSetLocale(TestCase):
